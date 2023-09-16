347c9c8bde4f01f648f5dc55065d6efde4b23b05
Veerachai <Mju6204107372@mju.ac.th>
2023-09-16 14:05:53 +0700

    14.05

--- pages/register/step2.vue
+++ pages/register/step2.vue
@@ -77,7 +77,7 @@ export default {
   data() {
     return {
       form: {
-        lineid:'0001',
+        lineid:this.$store.getters.getLine.userId,
         firstname : this.$store.getters.getRegister.firstname,
         lastname : this.$store.getters.getRegister.lastname,
         email: this.$store.getters.getRegister.email,
