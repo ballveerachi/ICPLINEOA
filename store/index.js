5303ec5eaf2cad69164323d94729afc185130c4f
Veerachai <Mju6204107372@mju.ac.th>
2023-09-27 11:08:07 +0700

    27/09/2566

--- store/index.js
+++ store/index.js
@@ -7,11 +7,11 @@ export const state = () =>({
   line:{
     pictureUrl: null,
     displayName: null,
-    // userId: null,
+    userId: null,
   },
 
   register: {
-    member_id:'',
+    member_id:'5555555',
     fullname:'',
     firstname:'',
     lastname:'',
