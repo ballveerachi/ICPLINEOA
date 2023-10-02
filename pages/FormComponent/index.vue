839b7623a8d82ef4b5043daa8bf84f23c959124d
Veerachai <Mju6204107372@mju.ac.th>
2023-10-03 01:03:44 +0700

    3/10/2566/0:48

--- pages/FormComponent/index.vue
+++ pages/FormComponent/index.vue
@@ -203,7 +203,7 @@ export default {
       employees: Array,
       employees_: Array,
       employee: {
-        id: this.$store.getters.myMember_id.member_id,
+        id: this.$store.getters.getRegister.member_id,
         name: this.$store.getters.getRegister.full_name,
         study_faculty: '',
         university: '',
