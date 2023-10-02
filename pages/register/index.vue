c876b0d2c6d533cc331fa67ffb0decec3299415e
Veerachai <Mju6204107372@mju.ac.th>
2023-10-02 23:38:23 +0700

    22.46

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -70,9 +70,9 @@ export default {
     liff.ready.then(() => {
       if (liff.isLoggedIn()) {
         liff.getProfile().then((profile) => {
-          this.isDone();
           this.form.member_id = profile.userId;
           this.$store.dispatch("setLine", profile);
+          // this.isDone();
           console.log("ข้อมูลจากLine", profile);
           console.log("LineID", profile.userId);
         });
