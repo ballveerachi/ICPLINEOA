7f68cb1986ad8ecbfd8f539095ebc8f75e15b74f
Veerachai <Mju6204107372@mju.ac.th>
2023-10-02 21:54:32 +0700

    2/10/2566/21.54

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -93,7 +93,17 @@ export default {
   },
   methods: {
     isDone() {
-
+      axios
+          .get("http://localhost/ICPScoreCard/api-member.php", {
+            action: "insert_register",
+            member_id: this.form.member_id,
+            full_name: this.form.full_name,
+          })
+          .then((res) => {
+           if(res.data != null){
+            this.$router.push('/register/done');
+           }
+          })
     },
 
     validate() {
