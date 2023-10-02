5a0f7c927ee7228d1efedce49433643a94198e2b
Veerachai <Mju6204107372@mju.ac.th>
2023-10-03 01:13:58 +0700

    3/10/2566/0:48

--- pages/FormPlanCareer/index.vue
+++ pages/FormPlanCareer/index.vue
@@ -139,7 +139,7 @@ export default {
       //Plan_Career_id	Employee_id	Name_Plan_Career Description
       planCareer: {
         Plan_Career_id: "",
-        Employee_id: this.$store.getters.myMember_id.id,
+        Employee_id: this.$store.getters.getRegister.member_id,
         career_id: "",
         career: "",
       },
--- store/index.js
+++ store/index.js
@@ -10,7 +10,7 @@ export const state = () =>({
     userId: null,
   },
   register: {
-    id:'',
+    user_id:'',
     member_id:'',
     full_name:'',
     firstname:'',
