4e15b02df4683c72baa473f7389447e09b5285b3
Veerachai <Mju6204107372@mju.ac.th>
2023-10-03 22:30:36 +0700

    22.04

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -65,7 +65,6 @@ export default {
           this.form.email = profile.userId;
           this.form.user_id = profile.userId;
           this.form.password = profile.userId;
-          this.form.member_id = '';
           this.$store.dispatch("setLine", profile);
           // this.isDone();
           console.log("ข้อมูลจากLine", profile);
@@ -128,7 +127,6 @@ export default {
             full_name: this.form.full_name,
           })
           .then((res) => {
-            this.form.member_id = res.data.member_id;
             console.log(res);
           })
           .catch(function (error) {
