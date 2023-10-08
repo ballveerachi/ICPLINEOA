00d12ea42f436de1f4fcb9c4b2a300177a4486e0
Veerachai <Mju6204107372@mju.ac.th>
2023-10-08 15:12:01 +0700

    8/10/2566

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -47,7 +47,7 @@ export default {
   },
   methods: {
     userid() {
-      this.$router.push("/login/LoginPage");
+      this.$router.push("/login/LoginEmail");
     },
     line() {
       this.$router.push("/register");
