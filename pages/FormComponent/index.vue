009689ae29ea0be85db49cec4530fa23c6076548
Veerachai <Mju6204107372@mju.ac.th>
2023-11-20 13:04:10 +0700

    20/11/2566

--- pages/FormComponent/index.vue
+++ pages/FormComponent/index.vue
@@ -51,6 +51,7 @@
               <v-col cols="12" md="6"
                 ><div>สาขาวิชา:</div>
                 <v-select
+                  required
                   size="4"
                   v-model="employee.study_faculty"
                   label="เลือก"
@@ -72,6 +73,7 @@
                 ><div>สำเร็จการศึกษาสถาบัน:</div>
                 <v-select
                   size="4"
+                  required
                   v-model="employee.university"
                   label="เลือก"
                   :items="universityTypes"
@@ -92,6 +94,7 @@
               <v-col
                 ><div>ความบกพร่อง:</div>
                 <v-select
+                  required
                   size="4"
                   v-model="employee.disability_type"
                   label="เลือก"
@@ -308,80 +311,80 @@ export default {
           })
           .then((res) => {
             liff
-          .sendMessages([
-            {
-              type: "flex",
-              altText: "this is a flex message",
-              contents: {
-                type: "bubble",
-                direction: "ltr",
-                header: {
-                  type: "box",
-                  layout: "vertical",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "บันทึกข้อมูล",
-                      weight: "bold",
-                      size:"lg",
-                      align: "center",
-                      contents: [],
+              .sendMessages([
+                {
+                  type: "flex",
+                  altText: "this is a flex message",
+                  contents: {
+                    type: "bubble",
+                    direction: "ltr",
+                    header: {
+                      type: "box",
+                      layout: "vertical",
+                      contents: [
+                        {
+                          type: "text",
+                          text: "บันทึกข้อมูล",
+                          weight: "bold",
+                          size: "lg",
+                          align: "center",
+                          contents: [],
+                        },
+                      ],
                     },
-                  ],
-                },
-                hero: {
-                  type: "image",
-                  url: "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
-                  size: "full",
-                  aspectRatio: "1.51:1",
-                  aspectMode: "fit",
-                },
-                body: {
-                  type: "box",
-                  layout: "vertical",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "xxxxx",
-                      align: "center",
-                      contents: [],
+                    hero: {
+                      type: "image",
+                      url: "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
+                      size: "full",
+                      aspectRatio: "1.51:1",
+                      aspectMode: "fit",
                     },
-                  ],
-                },
-                footer: {
-                  type: "box",
-                  layout: "horizontal",
-                  contents: [
-                    {
-                      type: "button",
-                      action: {
-                        type: "uri",
-                        label: "Back",
-                        uri: "https://linecorp.com",
-                      },
-                      color: "#CFC9CAFF",
-                      style: "primary",
+                    body: {
+                      type: "box",
+                      layout: "vertical",
+                      contents: [
+                        {
+                          type: "text",
+                          text: "xxxxx",
+                          align: "center",
+                          contents: [],
+                        },
+                      ],
                     },
-                    {
-                      type: "button",
-                      action: {
-                        type: "uri",
-                        label: "Next",
-                        uri: "https://linecorp.com",
-                      },
-                      style: "primary",
+                    footer: {
+                      type: "box",
+                      layout: "horizontal",
+                      contents: [
+                        {
+                          type: "button",
+                          action: {
+                            type: "uri",
+                            label: "Back",
+                            uri: "https://linecorp.com",
+                          },
+                          color: "#CFC9CAFF",
+                          style: "primary",
+                        },
+                        {
+                          type: "button",
+                          action: {
+                            type: "uri",
+                            label: "Next",
+                            uri: "https://linecorp.com",
+                          },
+                          style: "primary",
+                        },
+                      ],
                     },
-                  ],
+                  },
                 },
-              },
-            },
-          ])
-          .then(() => {
-            console.log("message sent");
-          })
-          .catch((err) => {
-            console.log("error", err);
-          });
+              ])
+              .then(() => {
+                console.log("message sent");
+              })
+              .catch((err) => {
+                console.log("error", err);
+              });
             console.log(res);
             this.resetForm();
             this.getAllUser();
