80de61e0eb1d9af5828188e58d2534657d452d54
Veerachai <Mju6204107372@mju.ac.th>
2023-11-22 17:26:18 +0700

    22/11/2566/17.26

--- pages/FormPlanCareer/index.vue
+++ pages/FormPlanCareer/index.vue
@@ -138,6 +138,20 @@
 import axios from "axios";
 
 export default {
+  mounted() {
+    liff.init({ liffId: "2000700725-PRVZgqqz" });
+    liff.ready.then(() => {
+      if (liff.isLoggedIn()) {
+        liff.getProfile().then((profile) => {
+          this.userId = profile.userId;
+          console.log("ข้อมูลจากLine", profile);
+          console.log("LineID", this.userId);
+        });
+      } else {
+        liff.login();
+      }
+    });
+  },
   name: "FormPlanCareer",
   data() {
     return {
@@ -158,7 +172,7 @@ export default {
     };
   },
   methods: {
-    //แก้ไขให้มันเป็นช่องว้่าง
+    //แก้ไขให้มันเป็นช่องว่าง
     resetForm() {
       this.status = "Save/บันทึก";
       this.isEdit = false;
@@ -214,6 +228,7 @@ export default {
           })
           .then((res) => {
             console.log(res);
+            this.sendMessages();
             this.resetForm();
             this.getAllUser();
           })
@@ -277,6 +292,199 @@ export default {
             console.log(error);
           });
       }
+    },
+    sendMessages() {
+      liff
+        .sendMessages([
+          {
+            type: "flex",
+            altText: "บันทึกข้อมูล",
+            contents: {
+              type: "bubble",
+              body: {
+                type: "box",
+                layout: "vertical",
+                spacing: "md",
+                // action: {
+                //   type: "uri",
+                //   label: "Action",
+                //   uri: "https://linecorp.com",
+                // },
+                contents: [
+                  {
+                    type: "text",
+                    text: "บันทึกข้อมูล",
+                    weight: "bold",
+                    size: "lg",
+                    align: "center",
+                    contents: [],
+                  },
+                  {
+                    type: "box",
+                    layout: "vertical",
+                    spacing: "sm",
+                    contents: [
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "ID",
+                            size: "sm",
+                            color: "#000000FF",
+                            margin: "sm",
+                            contents: [],
+                          },
+                          {
+                            type: "text",
+                            text: this.employee.id,
+                            weight: "regular",
+                            size: "sm",
+                            color: "#000000FF",
+                            align: "end",
+                            contents: [],
+                          },
+                        ],
+                      },
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "ชื่อผู้ใช้",
+                            weight: "regular",
+                            size: "sm",
+                            color: "#000000FF",
+                            margin: "sm",
+                            contents: [],
+                          },
+                          {
+                            type: "text",
+                            text: this.employee.name,
+                            size: "sm",
+                            color: "#000000FF",
+                            align: "end",
+                            contents: [],
+                          },
+                        ],
+                      },
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "สาขาวิชา",
+                            weight: "regular",
+                            size: "sm",
+                            color: "#000000FF",
+                            margin: "sm",
+                            contents: [],
+                          },
+                          {
+                            type: "text",
+                            text: this.employee.study_faculty,
+                            weight: "regular",
+                            size: "sm",
+                            color: "#000000FF",
+                            align: "end",
+                            contents: [],
+                          },
+                        ],
+                      },
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "สถาบันการศึกษา",
+                            weight: "regular",
+                            size: "sm",
+                            color: "#000000FF",
+                            margin: "sm",
+                            contents: [],
+                          },
+                          {
+                            type: "text",
+                            text: this.employee.university,
+                            size: "sm",
+                            color: "#000000FF",
+                            align: "end",
+                            contents: [],
+                          },
+                        ],
+                      },
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "ความบกพร่อง",
+                            weight: "regular",
+                            size: "sm",
+                            color: "#000000FF",
+                            margin: "sm",
+                            contents: [],
+                          },
+                          {
+                            type: "text",
+                            text: this.employee.disability_type,
+                            size: "sm",
+                            color: "#000000FF",
+                            align: "end",
+                            contents: [],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+              footer: {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "spacer",
+                    size: "xxl",
+                  },
+                  {
+                    type: "button",
+                    action: {
+                      type: "uri",
+                      label: "Next",
+                      uri: "https://elegant-buttercream-fd2e73.netlify.app/FormPlanCareer",
+                    },
+                    height: "sm",
+                    style: "primary",
+                  },
+                  {
+                    type: "button",
+                    action: {
+                      type: "uri",
+                      label: "Button",
+                      uri: "https://linecorp.com",
+                    },
+                    color: "#CFC9CAFF",
+                    height: "sm",
+                    style: "primary",
+                  },
+                ],
+              },
+            },
+          },
+        ])
+        .then(() => {
+          console.log("message sent");
+          liff.closeWindow();
+        })
+        .catch((err) => {
+          console.log("error", err);
+        });
     },
   },
   created() {
