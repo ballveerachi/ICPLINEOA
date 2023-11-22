018ae045204ddebf3a4a89b09f3b638f043d7c27
Veerachai <Mju6204107372@mju.ac.th>
2023-11-22 13:08:26 +0700

    22/11/2566

--- pages/FormComponent/index.vue
+++ pages/FormComponent/index.vue
@@ -313,69 +313,181 @@ export default {
             liff
               .sendMessages([
                 {
-                  type: "flex",
-                  altText: "this is a flex message",
-                  contents: {
-                    type: "bubble",
-                    direction: "ltr",
-                    header: {
-                      type: "box",
-                      layout: "vertical",
-                      contents: [
-                        {
-                          type: "text",
-                          text: "บันทึกข้อมูล",
-                          weight: "bold",
-                          size: "lg",
-                          align: "center",
-                          contents: [],
-                        },
-                      ],
+                  type: "bubble",
+                  body: {
+                    type: "box",
+                    layout: "vertical",
+                    spacing: "md",
+                    action: {
+                      type: "uri",
+                      label: "Action",
+                      uri: "https://linecorp.com",
                     },
-                    hero: {
-                      type: "image",
-                      url: "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
-                      size: "full",
-                      aspectRatio: "1.51:1",
-                      aspectMode: "fit",
-                    },
-                    body: {
-                      type: "box",
-                      layout: "vertical",
-                      contents: [
-                        {
-                          type: "text",
-                          text: "xxxxx",
-                          align: "center",
-                          contents: [],
-                        },
-                      ],
-                    },
-                    footer: {
-                      type: "box",
-                      layout: "horizontal",
-                      contents: [
-                        {
-                          type: "button",
-                          action: {
-                            type: "uri",
-                            label: "Back",
-                            uri: "https://linecorp.com",
+                    contents: [
+                      {
+                        type: "text",
+                        text: "บันทึกข้อมูล",
+                        weight: "bold",
+                        size: "xl",
+                        align: "center",
+                        contents: [],
+                      },
+                      {
+                        type: "box",
+                        layout: "vertical",
+                        spacing: "sm",
+                        contents: [
+                          {
+                            type: "box",
+                            layout: "baseline",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "ID",
+                                size: "md",
+                                color: "#000000FF",
+                                margin: "sm",
+                                contents: [],
+                              },
+                              {
+                                type: "text",
+                                text: "400kcl",
+                                weight: "regular",
+                                size: "md",
+                                color: "#000000FF",
+                                align: "end",
+                                contents: [],
+                              },
+                            ],
                           },
-                          color: "#CFC9CAFF",
-                          style: "primary",
-                        },
-                        {
-                          type: "button",
-                          action: {
-                            type: "uri",
-                            label: "Next",
-                            uri: "https://linecorp.com",
+                          {
+                            type: "box",
+                            layout: "baseline",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "ชื่อผู้ใช้",
+                                weight: "regular",
+                                size: "md",
+                                color: "#000000FF",
+                                margin: "sm",
+                                contents: [],
+                              },
+                              {
+                                type: "text",
+                                text: "400kcl",
+                                size: "md",
+                                color: "#000000FF",
+                                align: "end",
+                                contents: [],
+                              },
+                            ],
                           },
-                          style: "primary",
+                          {
+                            type: "box",
+                            layout: "baseline",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "สาขาวิชา",
+                                weight: "regular",
+                                size: "md",
+                                color: "#000000FF",
+                                margin: "sm",
+                                contents: [],
+                              },
+                              {
+                                type: "text",
+                                text: "400kcl",
+                                weight: "regular",
+                                size: "md",
+                                color: "#000000FF",
+                                align: "end",
+                                contents: [],
+                              },
+                            ],
+                          },
+                          {
+                            type: "box",
+                            layout: "baseline",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "สถาบันการศึกษา",
+                                weight: "regular",
+                                size: "md",
+                                color: "#000000FF",
+                                margin: "sm",
+                                contents: [],
+                              },
+                              {
+                                type: "text",
+                                text: "400kcl",
+                                size: "md",
+                                color: "#000000FF",
+                                align: "end",
+                                contents: [],
+                              },
+                            ],
+                          },
+                          {
+                            type: "box",
+                            layout: "baseline",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "ความบกพร่อง",
+                                weight: "regular",
+                                size: "md",
+                                color: "#000000FF",
+                                margin: "sm",
+                                contents: [],
+                              },
+                              {
+                                type: "text",
+                                text: "400kcl",
+                                size: "md",
+                                color: "#000000FF",
+                                align: "end",
+                                contents: [],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                  footer: {
+                    type: "box",
+                    layout: "vertical",
+                    contents: [
+                      {
+                        type: "spacer",
+                        size: "xxl",
+                      },
+                      {
+                        type: "button",
+                        action: {
+                          type: "uri",
+                          label: "Next",
+                          uri: "https://linecorp.com",
                         },
-                      ],
-                    },
+                        color: "#2BEB0FFF",
+                        height: "sm",
+                        style: "primary",
+                      },
+                      {
+                        type: "button",
+                        action: {
+                          type: "uri",
+                          label: "Button",
+                          uri: "https://linecorp.com",
+                        },
+                        color: "#F8F4F4FF",
+                        height: "sm",
+                        style: "secondary",
+                      },
+                    ],
                   },
                 },
               ])
@@ -452,6 +564,7 @@ export default {
           });
       }
     },
+    sendMessages() {},
   },
   created() {
     this.getAllUser();
