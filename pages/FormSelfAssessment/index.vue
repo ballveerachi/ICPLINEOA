7409329ac36edb13cbe07356677f873c5f3e0674
Veerachai <Mju6204107372@mju.ac.th>
2023-12-05 21:30:48 +0700

    5/12/2566

--- pages/FormSelfAssessment/index.vue
+++ pages/FormSelfAssessment/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-app-bar  dense flat dark>
-      <v-toolbar-title>{{pageTitle}}</v-toolbar-title>
+    <v-app-bar dense flat dark>
+      <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
     </v-app-bar>
     <v-card
       class="mx-auto pa-12 pb-8"
@@ -44,19 +44,18 @@
                     :required="true"
                     @change="getQualification()"
                     :items="careers"
-
                     item-value="Plan_Career_id"
                     item-text="career"
                   >
-                  <template v-slot:prepend-item>
-                    <v-list-item>
-                      <v-list-item-content>
-                        <v-list-item-title>
-                          <span class="grey--text">กำหนดรหัสอาชีพ:</span>
-                        </v-list-item-title>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </template>
+                    <template v-slot:prepend-item>
+                      <v-list-item>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            <span class="grey--text">กำหนดรหัสอาชีพ:</span>
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </template>
                     <template v-slot:selection="{ item }">
                       <span> {{ item.Plan_Career_id }}-{{ item.career }}</span>
                     </template>
@@ -64,14 +63,13 @@
                       <span> {{ item.Plan_Career_id }}-{{ item.career }}</span>
                     </template>
                   </v-select>
-
                 </v-col>
               </v-row>
               <v-row>
                 <v-col>
                   <div>Qualification-ID/รหัสคุณสมบัติ</div>
                   <v-select
-                  :size="4"
+                    :size="4"
                     v-model="selfAssessment.qa_plan_career_id"
                     :items="career_qualifications"
                     label="เลือก"
@@ -79,15 +77,15 @@
                     item-text="qualification_name"
                     variant="outlined"
                   >
-                  <template v-slot:prepend-item>
-                    <v-list-item>
-                      <v-list-item-content>
-                        <v-list-item-title>
-                          <span class="grey--text">กำหนดรหัสคุณสมบัติ:</span>
-                        </v-list-item-title>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </template>
+                    <template v-slot:prepend-item>
+                      <v-list-item>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            <span class="grey--text">กำหนดรหัสคุณสมบัติ:</span>
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </template>
                     <template v-slot:selection="{ item }">
                       <span
                         >{{ item.qa_plan_career_id }} -
@@ -106,8 +104,7 @@
 
               <v-row>
                 <v-col>
-                <div>AssessmentDate/วันที่ประเมินตนเอง:</div>
-
+                  <div>AssessmentDate/วันที่ประเมินตนเอง:</div>
 
                   <v-dialog
                     ref="dialog"
@@ -119,22 +116,30 @@
                     <template v-slot:activator="{ on, attrs }">
                       <v-text-field
                         v-model="selfAssessment.self_assessment_date"
-
                         prepend-icon="mdi-calendar"
                         readonly
                         v-bind="attrs"
                         v-on="on"
                       ></v-text-field>
                     </template>
-                    <v-date-picker v-model="selfAssessment.self_assessment_date" scrollable>
+                    <v-date-picker
+                      v-model="selfAssessment.self_assessment_date"
+                      scrollable
+                    >
                       <v-spacer></v-spacer>
-                      <v-btn text color="primary" @click="modal_self_assessment_date = false">
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="modal_self_assessment_date = false"
+                      >
                         Cancel
                       </v-btn>
                       <v-btn
                         text
                         color="primary"
-                        @click="$refs.dialog.save(selfAssessment.self_assessment_date)"
+                        @click="
+                          $refs.dialog.save(selfAssessment.self_assessment_date)
+                        "
                       >
                         OK
                       </v-btn>
@@ -151,20 +156,23 @@
                     v-model="selfAssessment.perform_id"
                     :required="true"
                     :items="performs"
+                    return-object
                     label="เลือก"
-
                     item-value="perform_id"
                     item-text="perform_name"
+                    @change="(val) => onPerformChange(val)"
                   >
-                  <template v-slot:prepend-item>
-                    <v-list-item>
-                      <v-list-item-content>
-                        <v-list-item-title>
-                          <span class="grey--text">ระดับการประเมินตนเอง:</span>
-                        </v-list-item-title>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </template>
+                    <template v-slot:prepend-item>
+                      <v-list-item>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            <span class="grey--text"
+                              >ระดับการประเมินตนเอง:</span
+                            >
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </template>
                     <template v-slot:selection="{ item }">
                       <span> {{ item.perform_name }}</span>
                     </template>
@@ -199,47 +207,46 @@
             </v-container>
           </v-form>
           <v-card class="mx-auto table" max-width="800px" cols="12" md="6">
-      <v-card text="..." variant="tonal">แก้ไขข้อมูล</v-card>
-      <v-row>
-        <v-col>SA-ID</v-col>
-        <v-col>QA-ID</v-col>
-        <v-col>Assessment Date</v-col>
-        <v-col>Self Assessment</v-col>
-        <v-col>Action</v-col>
-      </v-row>
-      <v-row
-        v-for="row in selfAssessments"
-        :key="row.self_assessment_id"
-        cols="12"
-        md="6"
-      >
-        <v-col>{{ row.self_assessment_id }}</v-col>
-        <v-col>{{ row.qa_plan_career_id }}</v-col>
-        <v-col>{{ row.self_assessment_date }}</v-col>
-        <v-col>{{ row.perform_id }}</v-col>
+            <v-card text="..." variant="tonal">แก้ไขข้อมูล</v-card>
+            <v-row>
+              <v-col>SA-ID</v-col>
+              <v-col>QA-ID</v-col>
+              <v-col>Assessment Date</v-col>
+              <v-col>Self Assessment</v-col>
+              <v-col>Action</v-col>
+            </v-row>
+            <v-row
+              v-for="row in selfAssessments"
+              :key="row.self_assessment_id"
+              cols="12"
+              md="6"
+            >
+              <v-col>{{ row.self_assessment_id }}</v-col>
+              <v-col>{{ row.qa_plan_career_id }}</v-col>
+              <v-col>{{ row.self_assessment_date }}</v-col>
+              <v-col>{{ row.perform_id }}</v-col>
 
-        <v-col
-          ><v-btn @click="editUser(row.self_assessment_id)"
-            ><v-icon small class="mr-2"> mdi-pencil </v-icon></v-btn
-          >
-          <v-btn @click="deleteUser(row.self_assessment_id)"
-            ><v-icon small class="mr-2"> mdi-delete </v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-card>
+              <v-col
+                ><v-btn @click="editUser(row.self_assessment_id)"
+                  ><v-icon small class="mr-2"> mdi-pencil </v-icon></v-btn
+                >
+                <v-btn @click="deleteUser(row.self_assessment_id)"
+                  ><v-icon small class="mr-2"> mdi-delete </v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
         </v-container>
       </v-main>
     </v-card>
     <!-- data-table -->
-
 
     <!-- /data-table -->
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 import liff from "@line/liff";
 export default {
   mounted() {
@@ -256,12 +263,12 @@ export default {
       }
     });
   },
-  name: 'FormSelfAssessment',
+  name: "FormSelfAssessment",
   data() {
     return {
-      pageTitle: 'ประเมินตัวเอง',
+      pageTitle: "ประเมินตัวเอง",
       modal_self_assessment_date: false,
-      message: 'Form Self Acessment',
+      message: "Form Self Acessment",
       currentYear: new Date().getFullYear(),
       performs: [],
       selfAssessments: [],
@@ -269,191 +276,195 @@ export default {
       careers: [],
       career_qualifications: [],
       employee_id: this.$store.getters.myMember_id,
-      planCareerId: '',
+      planCareerId: "",
       selfAssessment: {
-        self_assessment_id: '',
-        qualificationId: '',
-        qualification_name: '',
-        perform_id: '',
-        perform_name: '',
+        self_assessment_id: "",
+        qualificationId: "",
+        qualification_name: "",
+        perform_id: "",
+        perform_name: "",
         self_assessment_date: new Date(
           Date.now() - new Date().getTimezoneOffset() * 60000
         )
           .toISOString()
           .substr(0, 10),
-        planCareerId: '',
-        Plan_Career_id: '',
-        qa_plan_career_id: '',
+        planCareerId: "",
+        Plan_Career_id: "",
+        qa_plan_career_id: "",
+      },
+      Line:{
+        PerformName: " ",
+
       },
       isEdit: false,
-      status: 'Save/บันทึก',
-    }
+      status: "Save/บันทึก",
+    };
   },
   methods: {
     resetForm() {
-      this.status = 'Save/บันทึก'
-      this.isEdit = false
-      console.log('ยกเลิกการบันทึกข้อมูล')
+      this.status = "Save/บันทึก";
+      this.isEdit = false;
+      console.log("ยกเลิกการบันทึกข้อมูล");
       // this.selfAssessment.selfAssessmentId = 0;
       // this.selfAssessment.qualificationId = 0;
-      this.selfAssessment.qa_plan_career_id = ''
-      this.selfAssessment.Plan_Career_id = ''
-      this.selfAssessment.self_assessment_date = ''
-      this.selfAssessment.perform_id = ''
+      this.selfAssessment.qa_plan_career_id = "";
+      this.selfAssessment.Plan_Career_id = "";
+      this.selfAssessment.self_assessment_date = "";
+      this.selfAssessment.perform_id = "";
     },
     getAllUser() {
-      console.log(' แสดงข้อมูลทั้งหมด ')
-      var self = this
+      console.log(" แสดงข้อมูลทั้งหมด ");
+      var self = this;
       axios
         .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
-          action: 'getall',
+          action: "getall",
         })
         .then(function (res) {
-          console.log(res)
-          self.selfAssessments = res.data
+          console.log(res);
+          self.selfAssessments = res.data;
         })
         .catch(function (error) {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
     submitForm() {
       if (!this.isEdit) {
-        console.log('บันทึกข้อมูล')
-        console.log('qualification:', this.selfAssessment)
+        console.log("บันทึกข้อมูล");
+        console.log("qualification:", this.selfAssessment);
         const newSelfAssessment = {
           self_assessment_id: this.selfAssessment.self_assessment_id,
           qa_plan_career_id: this.selfAssessment.qa_plan_career_id,
           self_assessment_date: this.selfAssessment.self_assessment_date,
-          perform_id: this.selfAssessment.perform_id,
-        }
-        this.$emit('saveData', newSelfAssessment)
+          perform_id: this.selfAssessment.perform_id.perform_id,
+        };
+        this.$emit("saveData", newSelfAssessment);
 
         axios
           .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
-            action: 'insert',
+            action: "insert",
             self_assessment_id: this.selfAssessment.self_assessment_id,
             qa_plan_career_id: this.selfAssessment.qa_plan_career_id,
             self_assessment_date: this.selfAssessment.self_assessment_date,
-            perform_id: this.selfAssessment.perform_id,
+            perform_id: this.selfAssessment.perform_id.perform_id,
           })
           .then((res) => {
-            console.log(res)
+            console.log(res);
             this.sendMessages();
             this.resetForm();
             this.getAllUser();
           })
           .catch(function (error) {
-            console.log(error)
-          })
+            console.log(error);
+          });
       } else {
         axios
           .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
-            action: 'update',
+            action: "update",
             self_assessment_id: this.selfAssessment.self_assessment_id,
             qa_plan_career_id: this.selfAssessment.qa_plan_career_id,
             self_assessment_date: this.selfAssessment.self_assessment_date,
-            perform_id: this.selfAssessment.perform_id,
+            perform_id: this.selfAssessment.perform_id.perform_id,
           })
           .then((response) => {
-            console.log(response)
-            this.resetForm()
-            this.getAllUser()
+            console.log(response);
+            this.resetForm();
+            this.getAllUser();
           })
           .catch(function (error) {
-            console.log(error)
-          })
+            console.log(error);
+          });
       }
     },
     getPerform() {
-      console.log(' ค่าประเมินตนเอง ')
-      var self = this
+      console.log(" ค่าประเมินตนเอง ");
+      var self = this;
       axios
         .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
-          action: 'getPerform',
+          action: "getPerform",
         })
         .then(function (res) {
-          self.performs = res.data
-          console.log('perform:', self.performs)
+          self.performs = res.data;
+          console.log("perform:", self.performs);
         })
         .catch(function (error) {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
     getCareer() {
-      console.log(' ข้อมูลอาชีพ ')
-      var self = this
+      console.log(" ข้อมูลอาชีพ ");
+      var self = this;
       axios
         .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
-          action: 'getEmpCareer',
+          action: "getEmpCareer",
           employee_id: this.employee_id,
         })
         .then(function (res) {
-          self.careers = res.data
-          console.log('careers:', self.careers)
+          self.careers = res.data;
+          console.log("careers:", self.careers);
         })
         .catch(function (error) {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
     getQualification() {
-      console.log('แผนอาชีพ', this.selfAssessment.Plan_Career_id)
-      var self = this
+      console.log("แผนอาชีพ", this.selfAssessment.Plan_Career_id);
+      var self = this;
       axios
         .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
-          action: 'getCareer_Qualifiation',
+          action: "getCareer_Qualifiation",
 
           Plan_Career_id: this.selfAssessment.Plan_Career_id,
         })
         .then(function (res) {
-          console.log('แก้ไขแผนอาชีพ+++', res.data)
-          self.career_qualifications = res.data
+          console.log("แก้ไขแผนอาชีพ+++", res.data);
+          self.career_qualifications = res.data;
         })
         .catch(function (error) {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
     editUser(self_assessment_id) {
-      this.status = 'Update(อัพเดท)'
-      this.isEdit = true
-      var self = this
+      this.status = "Update(อัพเดท)";
+      this.isEdit = true;
+      var self = this;
       axios
         .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
-          action: 'edit',
+          action: "edit",
           self_assessment_id: self_assessment_id,
         })
         .then(function (response) {
-          console.log(response)
+          console.log(response);
           self.selfAssessment.self_assessment_id =
-            response.data.self_assessment_id
-          self.selfAssessment.Plan_Career_id = response.data.plan_career_id
-          self.getQualification()
+            response.data.self_assessment_id;
+          self.selfAssessment.Plan_Career_id = response.data.plan_career_id;
+          self.getQualification();
           self.selfAssessment.qa_plan_career_id =
-            response.data.qa_plan_career_id
-          ;(self.selfAssessment.perform_id = response.data.perform_id),
+            response.data.qa_plan_career_id;
+          (self.selfAssessment.perform_id = response.data.perform_id.perform_id),
             (self.selfAssessment.self_assessment_date =
               response.data.self_assessment_date),
-            (self.selfAssessments_ = response.data)
+            (self.selfAssessments_ = response.data);
         })
         .catch(function (error) {
-          console.log(error)
-        })
+          console.log(error);
+        });
     },
     deleteUser(self_assessment_id) {
-      if (confirm('คุณต้องการลบรหัส ' + self_assessment_id + ' หรือไม่ ?')) {
-        var self = this
+      if (confirm("คุณต้องการลบรหัส " + self_assessment_id + " หรือไม่ ?")) {
+        var self = this;
         axios
           .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
-            action: 'delete',
+            action: "delete",
             self_assessment_id: self_assessment_id,
           })
           .then(function (response) {
-            console.log(response)
-            self.resetForm()
-            self.getAllUser()
+            console.log(response);
+            self.resetForm();
+            self.getAllUser();
           })
           .catch(function (error) {
-            console.log(error)
-          })
+            console.log(error);
+          });
       }
     },
     sendMessages() {
@@ -603,7 +614,7 @@ export default {
                           },
                           {
                             type: "text",
-                            text: "ระดับการประเมิน",
+                            text: this.Line.PerformName,
                             size: "sm",
                             color: "#000000FF",
                             align: "end",
@@ -657,13 +668,18 @@ export default {
           console.log("error", err);
         });
     },
+    onPerformChange(val){
+      this.Line.PerformName = val.perform_name
+      console.log("onLevelName",val.perform_name);
+
+    },
   },
   created() {
-    this.getAllUser()
-    this.getCareer()
-    this.getPerform()
+    this.getAllUser();
+    this.getCareer();
+    this.getPerform();
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
