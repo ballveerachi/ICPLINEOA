d692545d7757f559d66bb3e1bb4cb6fb12af45a2
Veerachai <Mju6204107372@mju.ac.th>
2023-12-05 21:45:28 +0700

    5/12/2566

--- pages/FormPlanCareer/index.vue
+++ pages/FormPlanCareer/index.vue
@@ -272,7 +272,7 @@ export default {
           self.planCareer.Plan_Career_id = response.data.Plan_Career_id;
           self.planCareer.Employee_id = response.data.Employee_id;
           self.planCareer.career_id.career_id = response.data.career_id.career_id;
-          self.planCareer.career = response.data.career;
+          self.planCareer.career = response.data.career.career;
           self.planCareers_ = response.data;
           console.log("แก้ไขแผนอาชีพ:", response.data);
         })
