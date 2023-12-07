4cde9dfe46e280c041d071638681c81465a04162
Veerachai <Mju6204107372@mju.ac.th>
2023-12-07 15:55:34 +0700

    7/12/2566

--- pages/FormQualification/index.vue
+++ pages/FormQualification/index.vue
@@ -429,7 +429,7 @@ export default {
           .post("https://icp2022.net/ICPScoreCard/api-qa-plan-career.php", {
             action: 'update',
             qa_plan_career_id: this.qualification.qa_plan_career_id.qa_plan_career_id,
-            plan_career_id: this.qualification.planCareerId.plan_career_id.plan_career_id,
+            plan_career_id: this.qualification.planCareerId.Plan_Career_id,
             qualification_id: this.qualification.qualificationId.qualificationId,
             target_id: this.qualification.target_id.target_id,
             level_id: this.qualification.level_id.level_id,
