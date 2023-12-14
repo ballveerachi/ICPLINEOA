ed12c16be1d3b9c93040aba911a7e884c5958c23
Veerachai <Mju6204107372@mju.ac.th>
2023-12-14 21:41:09 +0700

    14/12/66

--- pages/FormPlan/index.vue
+++ pages/FormPlan/index.vue
@@ -388,7 +388,7 @@ export default {
         });
     },
     getQualification(val) {
-      console.log("เลิอก", val.career);
+      console.log("เลือก", this.val.career);
       console.log("แผนอาชีพ", val.Plan_Career_id);
       console.log("แผนอาชีพ", this.plan.Plan_Career_id);
       var self = this;
