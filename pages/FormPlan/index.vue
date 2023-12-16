4c686b5831fdeacd9c4b5ba8879557233c049451
Veerachai <Mju6204107372@mju.ac.th>
2023-12-17 00:55:42 +0700

    16/12/66

--- pages/FormPlan/index.vue
+++ pages/FormPlan/index.vue
@@ -393,17 +393,18 @@ export default {
     },
     getQualification(val) {
       this.Line.career = val.career;
-      // console.log("เลือก", val.career);
+      console.log("เลือก", val.career);
       console.log("แผนอาชีพ", this.plan.Plan_Career_id);
       var self = this;
       axios
         .post("https://icp2022.net/ICPScoreCard/api-career-qualification.php", {
           action: "getCareer_Qualifiation",
-          Plan_Career_id: this.plan.Plan_Career_id.Plan_Career_id, //แก้อันนี้
+          Plan_Career_id: this.plan.Plan_Career_id, //แก้อันนี้
         })
         .then(function (res) {
           console.log("ข้อมูลคุณสมบัติ", res.data);
-          self.career_qualifications = res.data;
+          //  self.career_qualifications = res.data;
+
         })
         .catch(function (error) {
           console.log(error);
@@ -481,7 +482,7 @@ export default {
           console.log("แก้ไขข้อมูล",response)
           self.plan.planId = response.data.planId
           self.plan.Plan_Career_id = response.data.plan_career_id
-          // self.getQualification()
+          self.getQualification()
           self.plan.qualification_name = response.data.qualification_name
           self.plan.qa_plan_career_id = response.data.qa_plan_career_id
           self.plan.doing = response.data.doing
