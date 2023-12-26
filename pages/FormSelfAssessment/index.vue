86561ca0b61943d0fc66e99c546bdee29f7a9399
Veerachai <Mju6204107372@mju.ac.th>
2023-12-26 12:58:24 +0700

    26/12/2566

--- pages/FormSelfAssessment/index.vue
+++ pages/FormSelfAssessment/index.vue
@@ -416,13 +416,13 @@ export default {
       //this.Line.career =val.career;
       //console.log("เลือก", val.career);
       //console.log("แผนอาชีพ", val.Plan_Career_id);
-      console.log("แผนอาชีพ", this.selfAssessment.Plan_Career_id.Plan_Career_id);
+      console.log("แผนอาชีพ", this.selfAssessment.Plan_Career_id);
       var self = this;
       axios
         .post("https://icp2022.net/ICPScoreCard/api-self-assessment.php", {
           action: "getCareer_Qualifiation",
 
-          Plan_Career_id: this.selfAssessment.Plan_Career_id.Plan_Career_id,
+          Plan_Career_id: this.selfAssessment.Plan_Career_id,
         })
         .then(function (res) {
           console.log("แก้ไขแผนอาชีพ", res.data);
