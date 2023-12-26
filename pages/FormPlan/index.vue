f85707d36335e56237f02d3e407874c07b8f9c25
Veerachai <Mju6204107372@mju.ac.th>
2023-12-26 13:59:02 +0700

    26/12/2566

--- pages/FormPlan/index.vue
+++ pages/FormPlan/index.vue
@@ -394,7 +394,7 @@ export default {
     getQualification(val) {
       //  this.Line.career = val.career;
       // console.log("เลือก", val.career);
-      console.log("แผนอาชีพ111", this.plan.Plan_Career_id);
+      console.log("แผนอาชีพ111", this.plan.Plan_Career_id.Plan_Career_id);
       var self = this;
       axios
         .post("https://icp2022.net/ICPScoreCard/api-career-qualification.php", {
