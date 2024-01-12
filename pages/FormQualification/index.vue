df6f1cea229221f34f95e0f20ded7ceae4d296c1
Veerachai <Mju6204107372@mju.ac.th>
2024-01-12 14:12:34 +0700

    12/1/2567

--- pages/FormQualification/index.vue
+++ pages/FormQualification/index.vue
@@ -288,7 +288,7 @@ export default {
       qualifications: [],
       qualifications_: [],
       // employee_id: this.$store.getters.myMember_id,
-      employee_id:121,
+      employee_id:" ",
       careerPath: " ",
       careers: [],
       career_qualifications: [],
@@ -356,7 +356,7 @@ export default {
       axios
         .post("https://icp2022.net/ICPScoreCard/api-qualification.php", {
           action: "getEmpCareer",
-          employee_id: this.employee_id,
+          employee_id: this.member_id,
         })
         .then(function (res) {
           self.careers = res.data;
