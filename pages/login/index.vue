16266358adc0f0fea2732b394c3085fb785e3f18
Veerachai <Mju6204107372@mju.ac.th>
2024-01-15 14:02:26 +0700

    15012567

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -76,7 +76,7 @@
               > -->
               <div class="text-right mt-2">
                 ยังไม่มีบัญชีใช่ไหม
-                <router-link to="/register/InEmail"
+                <router-link to="/InEmail"
                   >สมัครใช้งาน</router-link
                 >
               </div>
