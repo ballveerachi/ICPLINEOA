06166b9b9d92dd506597c1e69d29b87e94d64a18
Veerachai <Mju6204107372@mju.ac.th>
2024-01-19 10:44:43 +0700

    19/1/2567

--- pages/FormQualification/index.vue
+++ pages/FormQualification/index.vue
@@ -357,7 +357,7 @@ export default {
       axios
         .post("https://icp2022.net/ICPScoreCard/api-qualification.php", {
           action: "getEmpCareer",
-          employee_id: this.employee_id,
+          employee_id: em_id,
           // employee_id: this.member.id,
         })
         .then(function (res) {
